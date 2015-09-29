cd4f79e0a6844a9ace4e141b634ab92b0528757d
fabianormatias <fabianormatias@gmail.com>
2015-09-29 15:36:21 -0400

    add exemplos mkyong manipulacao arquivos

--- arquivos-properties/src/main/java/br/com/g6/leitura/escrita/exemplo1/CapturarEnderecoResources.java
+++ arquivos-properties/src/main/java/br/com/g6/leitura/escrita/exemplo1/CapturarEnderecoResources.java
@@ -1,11 +1,9 @@
 package br.com.g6.leitura.escrita.exemplo1;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Properties;
 
 //TODO Falta fazer escrever no arquivo
@@ -20,33 +18,12 @@ public class CapturarEnderecoResources {
 //			String propertiesPath = jarPath.getParentFile().getAbsolutePath();
 //			System.out.println(" propertiesPath-" + propertiesPath);
 			
-			File jarPath = new File(CapturarEnderecoResources.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-			String propertiesPath = jarPath.getParentFile().getAbsolutePath();
-			System.out.println(" propertiesPath-" + propertiesPath);
-			
-			URL url = CapturarEnderecoResources.class.getClassLoader().getResource("exemplo1.properties");
-			System.out.println("Endereco: " + url);
-
-			InputStream is = url.openStream();
-			prop.load(is);
-			
-			String login1;
-			String host1;
-			String password1;
-			login1 = prop.getProperty("prop.server.login");
-			host1 = prop.getProperty("prop.server.host");
-			password1 = prop.getProperty("prop.server.password");
-			System.out.println("Login = " + login1);
-			System.out.println("Host = " + host1);
-			System.out.println("Password = " + password1);
-			
-			// --------------
+//			URL url = CapturarEnderecoResources.class.getClassLoader().getResource("exemplo1.properties");
 			
 			prop.setProperty("prop.server.login", "fabiano");
 			
-//			OutputStream os = url.;
-			
-			FileOutputStream saida = new FileOutputStream(url.toString());
+			//TODO Ele não acha o arquivo
+			FileOutputStream saida = new FileOutputStream("exemplo1.properties");
 			prop.store(saida, "Alteração properties");
 			
 			String login;
--- arquivos-properties/src/main/java/com/g6/mkyong/leitura/exemplo3/App.java
+++ arquivos-properties/src/main/java/com/g6/mkyong/leitura/exemplo3/App.java
@@ -13,28 +13,22 @@ public class App {
 	}
 
 	private void printThemAll() {
-
 		Properties prop = new Properties();
 		InputStream input = null;
-
 		try {
-
 			String filename = "config.properties";
 			input = getClass().getClassLoader().getResourceAsStream(filename);
 			if (input == null) {
 				System.out.println("Sorry, unable to find " + filename);
 				return;
 			}
-
 			prop.load(input);
-
 			Enumeration<?> e = prop.propertyNames();
 			while (e.hasMoreElements()) {
 				String key = (String) e.nextElement();
 				String value = prop.getProperty(key);
 				System.out.println("Key : " + key + ", Value : " + value);
 			}
-
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		} finally {
